c4611cdc427056bf942952b59546e55f3740f6b7
Ankit.Jain4 <ankit.jain4@ey.com>
2023-01-24 10:35:34 -0800

    first commit

---
+++ gpt3_script.py
@@ -0,0 +1,20 @@
+import openai
+
+# Add your OpenAI API key
+openai.api_key = "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"
+
+def generate_text(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    message = completions.choices[0].text
+    return message.strip()
+
+generated_text = generate_text("Write a short story about a robot who wants to be human")
+print(generated_text)
